13a808cc7af40d66ab339154c052e61c1ac55b90
Shahriar Azam Khan <shahriarazamkhan@gmail.com>
2025-11-04 18:29:57 +0600

    Nested Schema

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
 from fastapi import FastAPI, HTTPException
-from enum import Enum
+from schemas import GenreURLChoices, Band
 
 
 app = FastAPI()
 
-class GenreURLChoices(Enum):
-     ROCK = "rock"
-     ELECTRONIC = "electronic"
-     METAL = "metal"
-     HIP_HOP = "hip-hop"
 
 
 
@@ -16,7 +11,9 @@ BANDS = [
   {"id" : 1, "name" : "The Kinks" , "genre" : "Rock"},    
   { "id": 2, "name": "The Beatles", "genre": "Rock" },
   { "id": 3, "name": "Pink Floyd", "genre": "Hip-hop" },
-  { "id": 4, "name": "The Rolling Stones", "genre": "Metal" },
+  { "id": 4, "name": "The Rolling Stones", "genre": "Metal" , 'albums' : [
+       {'title' : 'Master of Reality', 'release_date' : '1971-07-21'}
+  ]},
   { "id": 5, "name": "Led Zeppelin", "genre": "Rock" },
   { "id": 6, "name": "Queen", "genre": "Electronic" }
 ]
@@ -24,17 +21,21 @@ BANDS = [
 
 
 @app.get('/bands')
-async def bands() -> list:
-    return BANDS
+async def bands() -> list[Band]:
+    return [
+         Band(**b) for b in BANDS
+    ]
+
 
 
 @app.get('/bands/{band_id}', status_code=200) 
-async def band(band_id : int) -> dict:
-    band = next((b for b in BANDS if b['id'] == band_id), None)
+async def band(band_id : int) -> Band:
+    band = next((Band(**b) for b in BANDS if b['id'] == band_id), None)
     if band is None:
         raise HTTPException(status_code=404, detail="Band with this id not found")
     return band
     
+
 
 @app.get('/bands/genre/{genre}', status_code=200)
 async def bands_for_genre(genre : GenreURLChoices) -> list[dict]:
