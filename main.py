c29d285042cf998bef1149eb0820ffec50b2900c
Shahriar Azam Khan <shahriarazamkhan@gmail.com>
2025-11-04 21:06:20 +0600

    RequestBody and PostRequest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, HTTPException
-from schemas import GenreURLChoices, Band
+from schemas import GenreURLChoices, BandBase, BandCreate, BandWithID
 
 
 app = FastAPI()
@@ -14,7 +14,7 @@ BANDS = [
   { "id": 4, "name": "The Rolling Stones", "genre": "Metal" , 'albums' : [
        {'title' : 'Master of Reality', 'release_date' : '1971-07-21'}
   ]},
-  { "id": 5, "name": "Led Zeppelin", "genre": "metal" },
+  { "id": 5, "name": "Led Zeppelin", "genre": "Metal" },
   { "id": 6, "name": "Queen", "genre": "Electronic" }
 ]
 
@@ -23,11 +23,11 @@ BANDS = [
 @app.get('/bands')
 async def bands(genre : GenreURLChoices | None = None,
                 has_albums : bool = False 
-                ) -> list[Band]:
-    band_list = [Band(**b) for b in BANDS]
+                ) -> list[BandWithID]:
+    band_list = [BandWithID(**b) for b in BANDS]
     if genre:
          band_list = [
-              b for b in band_list if b.genre.lower() == genre.value
+              b for b in band_list if b.genre.value.lower() == genre.value
          ]
     if has_albums:
          band_list = [
@@ -38,8 +38,8 @@ async def bands(genre : GenreURLChoices | None = None,
 
 
 @app.get('/bands/{band_id}', status_code=200) 
-async def band(band_id : int) -> Band:
-    band = next((Band(**b) for b in BANDS if b['id'] == band_id), None)
+async def band(band_id : int) -> BandWithID:
+    band = next((BandWithID(**b) for b in BANDS if b['id'] == band_id), None)
     if band is None:
         raise HTTPException(status_code=404, detail="Band with this id not found")
     return band
@@ -51,3 +51,10 @@ async def bands_for_genre(genre : GenreURLChoices) -> list[dict]:
       return [
            b for b in BANDS if b['genre'].lower() == genre.value
       ]
+
+@app.post('/bands')
+async def create_band(band_data : BandCreate) -> BandWithID:
+     id = BANDS[-1]['id'] + 1
+     band = BandWithID(id=id, **band_data.model_dump()).model_dump()
+     BANDS.append(band)
+     return band
